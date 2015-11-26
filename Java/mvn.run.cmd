c81147e38fc5abf593a625c326dfe03546232ce6
amitnegi09 <amitnegi054@gmail.com>
2015-11-26 13:04:34 +0530

    Run cmd
    cmd

---
+++ Java/mvn.run.cmd
@@ -0,0 +1,5 @@
+
+SET JAVA_HOME=C:\Program Files\Java\jdk1.7.0_80
+
+set MAVEN_OPTS=-Xmx1324M
+mvn -P cargo.run -Dcargo.maven.containerId=tomcat7x -Dcargo.maven.containerUrl=http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.40/bin/apache-tomcat-7.0.40.zip -Drepo.path=D:\Java-Cms\Java\repository
